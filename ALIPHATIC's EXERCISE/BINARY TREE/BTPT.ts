583d52076b8b07456883d8cb03a93a060727dc21
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-17 20:00:48 +0100

    TypeScript Solution

---
+++ ALIPHATIC's EXERCISE/BINARY TREE/BTPT.ts
@@ -0,0 +1,60 @@
+/** Binary Tree Preorder Traversal
+
+Solution
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    let result: number[] = [];
+    if (root === null) return result;
+    let stack: TreeNode[] = [];
+    stack.push(root);
+    while (stack.length > 0) {
+        let node: TreeNode = stack.pop();
+        result.push(node.val);
+        if (node.right !== null) stack.push(node.right);
+        if (node.left !== null) stack.push(node.left);
+    }
+    return result;
+
+};
