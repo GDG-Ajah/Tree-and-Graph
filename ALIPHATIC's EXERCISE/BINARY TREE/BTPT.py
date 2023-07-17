52663213586bb1efa708f42774b9f6ae20723a16
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-17 19:57:11 +0100

    Solution to Python Exercise

--- ALIPHATIC's EXERCISE/BINARY TREE/BTPT.py
+++ ALIPHATIC's EXERCISE/BINARY TREE/BTPT.py
@@ -26,3 +26,35 @@
  
 
 # Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+            """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+            if root is None:
+                return []
+            
+            stack = [root]
+            result = []
+            
+            while stack:
+                node = stack.pop()
+                result.append(node.val)
+                
+                if node.right:
+                    stack.append(node.right)
+                if node.left:
+                    stack.append(node.left)
+                    
+            return result
