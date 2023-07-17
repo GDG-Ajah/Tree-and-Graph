b33532f1be79b4b2392611dea7234b69496746cd
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-17 19:59:27 +0100

    Javascript Solution

--- ALIPHATIC's EXERCISE/BINARY TREE/BTPT.js
+++ ALIPHATIC's EXERCISE/BINARY TREE/BTPT.js
@@ -29,3 +29,37 @@ The number of nodes in the tree is in the range [0, 100].
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    
+        let stack = [];
+        let result = [];
+        
+        if(root === null) return result;
+        
+        stack.push(root);
+        
+        while(stack.length > 0){
+            let node = stack.pop();
+            result.push(node.val);
+            if(node.right !== null) stack.push(node.right);
+            if(node.left !== null) stack.push(node.left);
+        }
+        
+        return result;
+        
+    }
+
+    
+    
