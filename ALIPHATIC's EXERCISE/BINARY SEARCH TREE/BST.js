9beb948bf955792de396c086f7da4c35bf8ed0fb
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-18 11:37:56 +0100

    JavaScript BST Solution

--- ALIPHATIC's EXERCISE/BINARY SEARCH TREE/BST.js
+++ ALIPHATIC's EXERCISE/BINARY SEARCH TREE/BST.js
@@ -8,3 +8,30 @@ The left subtree of a node contains only nodes with keys less than the node's ke
 The right subtree of a node contains only nodes with keys greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
 */ 
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    
+        const helper = (node, lower, upper) => {
+            if(node === null) return true;
+            
+            if(node.val <= lower || node.val >= upper) return false;
+            
+            return helper(node.left, lower, node.val) && helper(node.right, node.val, upper);
+        }
+        
+        return helper(root, -Infinity, Infinity);
+    
+};
