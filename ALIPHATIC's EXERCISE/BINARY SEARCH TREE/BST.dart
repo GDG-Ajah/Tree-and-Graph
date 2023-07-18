e0deee43b63fa4fa0b9562ddc5ee1828aa999fbd
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-18 11:32:54 +0100

    Solution to Dart BST

--- ALIPHATIC's EXERCISE/BINARY SEARCH TREE/BST.dart
+++ ALIPHATIC's EXERCISE/BINARY SEARCH TREE/BST.dart
@@ -8,3 +8,25 @@
 // The right subtree of a node contains only nodes with keys greater than the node's key.
 // Both the left and right subtrees must also be binary search trees.
  
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  bool isValidBST(TreeNode? root) {
+    bool isValidBSTHelper(TreeNode? root, int? min, int? max) {
+      if (root == null) return true;
+      if (min != null && root.val <= min) return false;
+      if (max != null && root.val >= max) return false;
+      return isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max);
+    }
+    return isValidBSTHelper(root, null, null);
+
+  }
+}
