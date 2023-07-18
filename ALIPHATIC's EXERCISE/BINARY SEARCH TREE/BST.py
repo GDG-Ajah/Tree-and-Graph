717cf03c8c327ca21348b6f3a4520d5e4ecd9745
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-18 11:40:01 +0100

    Python BST Solution

--- ALIPHATIC's EXERCISE/BINARY SEARCH TREE/BST.py
+++ ALIPHATIC's EXERCISE/BINARY SEARCH TREE/BST.py
@@ -8,3 +8,31 @@
 # The right subtree of a node contains only nodes with keys greater than the node's key.
 # Both the left and right subtrees must also be binary search trees.
  
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+
+        def helper(node, lower = float('-inf'), upper = float('inf')):
+            if not node:
+                return True
+            
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return helper(root)
+    
