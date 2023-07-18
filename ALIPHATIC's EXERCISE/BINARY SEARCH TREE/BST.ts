d677d0beba784bdbbb76e5a2ec889ce758162114
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-18 11:35:56 +0100

    Typescript BST Solution

--- ALIPHATIC's EXERCISE/BINARY SEARCH TREE/BST.ts
+++ ALIPHATIC's EXERCISE/BINARY SEARCH TREE/BST.ts
@@ -8,3 +8,29 @@ The left subtree of a node contains only nodes with keys less than the node's ke
 The right subtree of a node contains only nodes with keys greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
 */ 
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isValidBST(root: TreeNode | null): boolean {
+    return isValid(root, null, null);
+
+    function isValid(root: TreeNode | null, min: number | null, max: number | null): boolean {
+        if (root === null) return true;
+        if (min !== null && root.val <= min) return false;
+        if (max !== null && root.val >= max) return false;
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+    }
+
+};
